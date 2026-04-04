3dc57691fa1bddba792bdbe7166ad775f147f9b7
kyuwan-dmu <kyuwan@dongyang.ac.kr>
2026-04-05 08:21:48 +0900

    chore: spring boot project initialize

---
+++ src/main/java/com/ward/ward_app/WardAppApplication.java
@@ -0,0 +1,13 @@
+package com.ward.ward_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardAppApplication.class, args);
+	}
+
+}
